92f761fc493af42c07a1e5bfb55ecd8c8b116709
Joris <miisterone27@gmail.com>
2026-08-31 21:52:36 +0200

    fix(backend): add Validation Pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'prisma/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(env('PORT'));
 }
 bootstrap();
